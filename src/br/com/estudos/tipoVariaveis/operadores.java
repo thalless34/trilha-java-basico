418b9a58fcef2faaa5e0b17280374d229f1a0d76
thalless34 <th_allessobral0@icloud.com>
2024-07-05 20:03:18 -0300

    update

---
+++ src/br/com/estudos/tipoVariaveis/operadores.java
@@ -0,0 +1,27 @@
+package br.com.estudos.tipoVariaveis;
+
+public class operadores {
+
+	public static void main(String[] args) {
+
+		String nomeCompleto = "Tas " + "34";
+		
+		String concatenacao ="?"; 
+
+		concatenacao = 1+1+1+"1";
+		System.out.println(concatenacao);
+
+		concatenacao = 1+"1"+1+1;
+		System.out.println(concatenacao);
+
+		concatenacao = 1+"1"+1+"1";
+		System.out.println(concatenacao);
+
+		concatenacao = "1"+1+1+1;
+		System.out.println(concatenacao);
+
+		concatenacao = "1"+(1+1+1);
+		System.out.println(concatenacao);
+	}
+
+}
---
+++ src/br/com/estudos/tipoVariaveis/tiposVariaveis.java
@@ -0,0 +1,20 @@
+package br.com.estudos.tipoVariaveis;
+public class tiposVariaveis {
+
+	public static void main(String[] args) {
+
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+	
+		int num = 5;
+		
+		num = 10;
+		
+		System.out.println(num);
+		
+		final double PI = 3.14;
+	
+	}
+
+}
